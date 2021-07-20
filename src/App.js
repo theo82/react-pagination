2a603a00a0b186567675dc603573a28c031e07cd
theo82 <theoaristi53@gmail.com>
2021-07-20 17:20:35 +0300

    Pagination - Prev and Next Buttons

--- src/App.js
+++ src/App.js
@@ -14,6 +14,27 @@ function App() {
   const handlePage = (index) => {
     setPage(index)
   }
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1
+      if (nextPage > data.length - 1) {
+        nextPage = 0
+      }
+      return nextPage
+    })
+  }
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1
+      if (prevPage < 0) {
+        prevPage = data.length - 1
+      }
+      return prevPage
+    })
+  }
+
   return (
     <main>
       <div className='section-title'>
@@ -27,6 +48,9 @@ function App() {
           </div>
           {!loading && (
             <div className='btn-container'>
+              <button className='prev-btn' onClick={prevPage}>
+                prev
+              </button>
               {data.map((item, index) => {
                 return (
                   <button
@@ -40,6 +64,9 @@ function App() {
                   </button>
                 )
               })}
+              <button className='next-btn' onClick={nextPage}>
+                next
+              </button>
             </div>
           )}
         </section>
