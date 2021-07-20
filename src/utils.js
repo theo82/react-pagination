65ddaa2310a3c676350dbb88c503b2bb7745159a
theo82 <theoaristi53@gmail.com>
2021-07-20 11:12:56 +0300

    Pagination - Paginate Logic

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 9
+  const pages = Math.ceil(followers.length / itemsPerPage)
+
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage
+    return followers.slice(start, start + itemsPerPage)
+  })
+
+  console.log(newFollowers)
+}
 
 export default paginate
