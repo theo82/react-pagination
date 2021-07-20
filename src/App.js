93a458bbfe9d3e7431d5ea99872671173926fb87
theo82 <theoaristi53@gmail.com>
2021-07-20 09:33:05 +0300

    Pagination - Fetch/Display List

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './follower'
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch()
+
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className='underline'></div>
+        <section className='followers'>
+          <div className='container'>
+            {data.map((follower) => {
+              return <Follower key={follower.id} {...follower} />
+            })}
+          </div>
+        </section>
+      </div>
+    </main>
+  )
 }
 
 export default App
